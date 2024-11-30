0b40ead5ed86408098cd0d50f163ce450c8dc4a9
ArcidesFerrao <cidesferrao@gmail.com>
2024-12-01 00:36:20 +0200

    form validation with zod

--- src/app/emprego/vaga/_components/VagaForm.tsx
+++ src/app/emprego/vaga/_components/VagaForm.tsx
@@ -1,12 +1,48 @@
-import React from "react";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+import React, { useActionState } from "react";
+import { z } from "zod";
+
+const jobPostSchema = z.object({
+  title: z
+    .string()
+    .min(3, "Title must be at least 3 characters long")
+    .max(100, "Title cannot exceed 100 characters"),
+  description: z.string().min(10, "Description mst be at lt 10 characters"),
+  location: z.string().min(2, "Location is required"),
+  salary: z.string().optional(),
+  company: z
+    .string()
+    .min(2, "Company name must be at least 2 characters long")
+    .max(100, "Company name cannot exceed 100 characters"),
+  jobUrl: z.string().url("Invalid URL format").optional(),
+});
 
 export default function VagaForm() {
+  const [lastResult, action] = useActionState(addVaga, undefined);
+  const [form, fields] = useForm({
+    lastResult,
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: jobPostSchema });
+    },
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
+
   return (
-    <form className="p-4 flex flex-col gap-y-4 w-4/6 ">
+    <form
+      id={form.id}
+      onSubmit={form.onSubmit}
+      action={action}
+      className="p-4 flex flex-col gap-y-4 w-4/6 "
+    >
       <div>
         <label htmlFor="company">Company</label>
         <input type="text" name="company" />
       </div>
+      {fields.company.errors && (
+        <p className="errorText">{fields.company.errors}</p>
+      )}
       <div>
         <label htmlFor="logo">Logo</label>
         <input type="text" name="logo" />
