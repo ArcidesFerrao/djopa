96cb2f73a41710de9de40b833cdc890a04497c9a
ArcidesFerrao <cidesferrao@gmail.com>
2025-03-19 07:57:50 +0200

    job details in job page

--- src/app/emprego/[id]/page.tsx
+++ src/app/emprego/[id]/page.tsx
@@ -15,27 +15,35 @@ export default async function JobPage({
   if (!id) return <p>Vaga nao encontrada</p>;
 
   return (
-    <main className="flex flex-col items-center gap-8 justify-center">
-      <section className="job-info">
-        <div className="job-post-header">
+    <main className="flex justify-between">
+      <section className="job-info h-fit flex flex-col gap-5">
+        <div className="job-post-header flex justify-between">
           <h2>{job?.title}</h2>
+          <button>Candidatar-me</button>
         </div>
-        <div className="job-post-details">
-          <div className="address-applications">
-            <p>{job?.location}, Mozambique</p>
-            <p>5 candidaturas</p>
+        <div className="address-applications flex gap-6">
+          <p>{job?.location}, Mozambique</p>
+          <p>MZN 60 000,00</p>
+          <p>Valido ate 25/03/2025</p>
+          <p>5 candidaturas</p>
+        </div>
+        <div className="about-job flex flex-col gap-5">
+          <div className="description">
+            <h3>Sobre a vaga</h3>
+            <p>{job?.description}</p>
           </div>
-          <div className="about-job">
-            <div className="description">
-              <h4>Sobre a vaga</h4>
-              <p>{job?.description}</p>
-            </div>
-            <div className="requirements">
-              <h4>Requisitos</h4>
-              <p>{job?.competencies}</p>
-            </div>
+          <div className="job-requirements">
+            <h3>Requisitos</h3>
+            <p>{job?.competencies}</p>
+          </div>
+          <div className="job-benefits">
+            <h3>Beneficios</h3>
+            <p>{job?.competencies}</p>
           </div>
         </div>
+      </section>
+      <section className="similar-jobs">
+        <div className="job-card"></div>
       </section>
     </main>
   );
